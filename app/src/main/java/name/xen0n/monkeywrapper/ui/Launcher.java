a168df817eaa5e4db68dd5c85fe87a7334fabd8e
Wang Xuerui <idontknw.wang@gmail.com>
2015-06-06 14:52:58 +0800

    Launcher: test check root status

--- app/src/main/java/name/xen0n/monkeywrapper/ui/Launcher.java
+++ app/src/main/java/name/xen0n/monkeywrapper/ui/Launcher.java
@@ -2,8 +2,10 @@ package name.xen0n.monkeywrapper.ui;
 
 import name.xen0n.monkeywrapper.R;
 import name.xen0n.monkeywrapper.app.MWBaseActivity;
+import name.xen0n.monkeywrapper.util.ShellUtils;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 import butterknife.InjectView;
 
 
@@ -11,6 +13,9 @@ public class Launcher extends MWBaseActivity {
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
+
+    @InjectView(R.id.textRootStatus)
+    TextView textRootStatus;
 
     @Override
     protected int getContentViewId(final Bundle icicle) {
@@ -22,6 +27,7 @@ public class Launcher extends MWBaseActivity {
         super.onCreate(icicle);
 
         setupToolbar(toolbar);
+        checkRootStatus();
     }
 
     public void onEvent(int dummy) {
@@ -29,5 +35,10 @@ public class Launcher extends MWBaseActivity {
 
     private void setupToolbar(final Toolbar toolbar) {
         toolbar.setTitle(R.string.app_name);
+    }
+
+    private void checkRootStatus() {
+        final boolean isRooted = ShellUtils.checkRooted();
+        textRootStatus.setText(isRooted ? "rooted" : "not rooted");
     }
 }
