e46bfe82313f672ae6a41612cd728c0c0911e9b6
Wang Xuerui <idontknw.wang@gmail.com>
2015-06-06 16:03:59 +0800

    Launcher: test check monkey status

--- app/src/main/java/name/xen0n/monkeywrapper/ui/Launcher.java
+++ app/src/main/java/name/xen0n/monkeywrapper/ui/Launcher.java
@@ -5,11 +5,14 @@ import name.xen0n.monkeywrapper.app.MWBaseActivity;
 import name.xen0n.monkeywrapper.util.ShellUtils;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.widget.TextView;
 import butterknife.InjectView;
 
 
 public class Launcher extends MWBaseActivity {
+
+    private static final String TAG = "Launcher";
 
     @InjectView(R.id.toolbar)
     Toolbar toolbar;
@@ -28,6 +31,7 @@ public class Launcher extends MWBaseActivity {
 
         setupToolbar(toolbar);
         checkRootStatus();
+        checkMonkeyStatus();
     }
 
     public void onEvent(int dummy) {
@@ -40,5 +44,16 @@ public class Launcher extends MWBaseActivity {
     private void checkRootStatus() {
         final boolean isRooted = ShellUtils.checkRooted();
         textRootStatus.setText(isRooted ? "rooted" : "not rooted");
+    }
+
+    private void checkMonkeyStatus() {
+        String out = null;
+        try {
+            out = ShellUtils.sudo("monkey");
+        } catch (Exception e) {
+            Log.e(TAG, "checkMonkeyStatus errored", e);
+        }
+
+        Log.d(TAG, "checkMonkeyStatus OK\n" + out);
     }
 }
