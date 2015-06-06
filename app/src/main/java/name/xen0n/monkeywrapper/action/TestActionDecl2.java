3dbfbbb414c2d831c88082993c586a378811905f
Wang Xuerui <idontknw.wang@gmail.com>
2015-06-07 07:11:38 +0800

    add test action declarations

---
+++ app/src/main/java/name/xen0n/monkeywrapper/action/TestActionDecl2.java
@@ -0,0 +1,11 @@
+package name.xen0n.monkeywrapper.action;
+
+import name.xen0n.monkeywrapper.action.sequence.TestSequence2Factory;
+
+
+public class TestActionDecl2 extends MWActionDecl {
+
+    public TestActionDecl2() {
+        super("com.tencent.mm", "", TestSequence2Factory.class);
+    }
+}
