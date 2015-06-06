22193723534f69872007cca18788e18332745be2
Wang Xuerui <idontknw.wang@gmail.com>
2015-06-06 20:44:59 +0800

    MWRequestActionEvent: fix incorrect event data

--- app/src/main/java/name/xen0n/monkeywrapper/action/MWRequestActionEvent.java
+++ app/src/main/java/name/xen0n/monkeywrapper/action/MWRequestActionEvent.java
@@ -3,27 +3,12 @@ package name.xen0n.monkeywrapper.action;
 public class MWRequestActionEvent {
 
     private final int actionId;
-    private final CharSequence targetPackage;
-    private final CharSequence targetClass;
 
-    public MWRequestActionEvent(
-            final int actionId,
-            final CharSequence targetPackage,
-            final CharSequence targetClass) {
+    public MWRequestActionEvent(final int actionId) {
         this.actionId = actionId;
-        this.targetPackage = targetPackage;
-        this.targetClass = targetClass;
     }
 
     public int getActionId() {
         return actionId;
-    }
-
-    public CharSequence getTargetPackage() {
-        return targetPackage;
-    }
-
-    public CharSequence getTargetClass() {
-        return targetClass;
     }
 }
